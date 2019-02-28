2bff1433935502e5af91eb6cc9755fbecf786502
yefcode <yefcode@gmail.com>
2019-02-28 04:15:46 -0500

    refactoring material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,3 @@
-import {A11yModule} from '@angular/cdk/a11y';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {ScrollingModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
 import {NgModule} from '@angular/core';
 import {
   MatAutocompleteModule,
@@ -47,11 +41,6 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [],
   exports: [
-    A11yModule,
-    CdkStepperModule,
-    CdkTableModule,
-    CdkTreeModule,
-    DragDropModule,
     MatAutocompleteModule,
     MatBadgeModule,
     MatBottomSheetModule,
@@ -86,8 +75,7 @@ import { CommonModule } from '@angular/common';
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    MatTreeModule,
-    ScrollingModule,
+    MatTreeModule
   ],
   imports: [
     CommonModule
